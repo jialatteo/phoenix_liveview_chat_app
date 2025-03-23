2f51687acdc9f9721ef918772548488564612bb9
Gerald Teo <geraldteo7@gmail.com>
2025-03-24 01:58:39 +0800

    Deploy application

--- config/prod.exs
+++ config/prod.exs
@@ -8,13 +8,13 @@ import Config
 config :chat, ChatWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
-config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Chat.Finch
+# config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Chat.Finch
 
 # Disable Swoosh Local Memory Storage
-config :swoosh, local: false
+# config :swoosh, local: false
 
 # Do not print debug messages in production
-config :logger, level: :info
+# config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
